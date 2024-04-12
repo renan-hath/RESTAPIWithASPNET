4d675d8a8e661ec41d754251897dad748d5c0005
Renan Hatherly <renan.hath@gmail.com>
2024-04-12 19:43:14 -0300

    Client scaffolding and directory structure rearranged
    - React client generated by create-react-app.
    - Project has been rearranged into two main folders: API and Client.
    - gitignore file has been updated for the client.

---
+++ Client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          React REST Client
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
